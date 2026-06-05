be01a9cf2bac24506a60c61914109ca10be1fee9
Natacha Payen <natacha.payen@arondor.com>
2026-06-05 15:27:44 +0200

    Add flowerDocsPlumeVersion constant FD-17430

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const flowerDocsArenderVersion = "2023.17.0";
 const flowerDocsOSVersion = "1.3.19";
 const flowerDocsRedisVersion = "6.2.12";
 const flowerDocsCompanionVersion = "2025.3.2";
-
+const flowerDocsPlumeVersion ="2026.0.0-SNAPSHOT";
 
 const getPdfFileName = (
     siteConfig: any,
